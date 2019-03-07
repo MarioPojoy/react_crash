c90c7e43a2fc51e49ea721dbd88dd368c6223fbb
Mario Pojoy <mariopc18@gmail.com>
2019-03-06 18:45:36 -0600

    React Crash Course

---
+++ src/components/pages/About.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+function About() {
+  return (
+    <React.Fragment>
+      <h1>About</h1>
+      <p>Todo App v1.0.0 REACT Crash Course</p>
+    </React.Fragment>
+  )
+}
+
+export default About;
