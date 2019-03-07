f22c542b592e21f203bd85d0301a667449faf502
Mario Pojoy <mariopc18@gmail.com>
2019-03-06 18:47:02 -0600

    Updates

---
+++ precache-manifest.e87f3cac34021418fdfdb3979e52b8dd.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "8084c186a6cccdecb750",
+    "url": "/react_crash/static/js/runtime~main.8084c186.js"
+  },
+  {
+    "revision": "669bdeb43b11f942d08c",
+    "url": "/react_crash/static/js/main.669bdeb4.chunk.js"
+  },
+  {
+    "revision": "fd6e8e9acdb20f71f911",
+    "url": "/react_crash/static/js/2.fd6e8e9a.chunk.js"
+  },
+  {
+    "revision": "669bdeb43b11f942d08c",
+    "url": "/react_crash/static/css/main.6cbfcd1c.chunk.css"
+  },
+  {
+    "revision": "6d696ee3fde4972fe8a647b2aa75bcaf",
+    "url": "/react_crash/index.html"
+  }
+];
